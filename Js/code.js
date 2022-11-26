68944944eae15e0b411a57c6f2077c3c11fe6388
Brogy-alt <brogangys2@gmail.com>
2022-11-26 10:59:59 +0200

    thanks John

--- Js/code.js
+++ Js/code.js
@@ -6,11 +6,20 @@ function display(arg) {
     output.value += arg;
 }
 
+function displayOp(arg) {
+    output.value += arg;
+    allowDecimal = true
+}
+
 const equalBtn = document.querySelector(".equal");
 equalBtn.addEventListener('click', ()=>{
     if (output !== ''){
-        // calculate if the is a value in the screen
+        // calculate if there is a value in the screen
         output.value = eval(output.value);
+
+        if (output.value.includes('.')) {
+            allowDecimal = false
+        } else allowDecimal = true
     }
 })
 
@@ -18,16 +27,17 @@ const clearBtn = document.querySelector(".clear");
 clearBtn.addEventListener('click', ()=>{
     if (output !== ''){
         output.value = "";
+        allowDecimal = true;
     }
 })
 
-// const dotBtn = document.querySelector(".dot");
 
-// dotBtn.addEventListener('click', function(){
-//     if (input != ".")
-//         if( output != true){
-//             output += ".";
-//             input = true;
-//         }else (let .dot = false;)
-// }
-// )
+let allowDecimal = true;
+
+function displayDecimal(value){
+    if (allowDecimal) {
+        output.value += value
+        allowDecimal = false
+    }
+}
+
